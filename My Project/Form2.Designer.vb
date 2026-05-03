897a78b3a9439b3996a4c44432ee2be8927a27f3
Emmanuel <golondrina.136510120196@depedqc.ph>
2026-05-03 23:56:46 +0800

    firstAdd project files.

--- My Project/Form2.Designer.vb
+++ My Project/Form2.Designer.vb
@@ -22,15 +22,366 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        ComboBox1 = New ComboBox()
+        Label3 = New Label()
+        ComboBox2 = New ComboBox()
+        Label4 = New Label()
+        ComboBox3 = New ComboBox()
+        Label5 = New Label()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        RadioButton4 = New RadioButton()
+        RadioButton5 = New RadioButton()
+        TextBox2 = New TextBox()
+        Label8 = New Label()
+        GroupBox1 = New GroupBox()
+        RadioButton9 = New RadioButton()
+        RadioButton10 = New RadioButton()
+        RadioButton6 = New RadioButton()
+        RadioButton8 = New RadioButton()
+        RadioButton7 = New RadioButton()
+        GroupBox2 = New GroupBox()
+        Label6 = New Label()
+        Button1 = New Button()
+        Label9 = New Label()
+        Button2 = New Button()
+        GroupBox1.SuspendLayout()
         SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 15F)
+        TextBox1.Location = New Point(12, 102)
+        TextBox1.Name = "TextBox1"
+        TextBox1.PlaceholderText = "Text here.."
+        TextBox1.Size = New Size(300, 41)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 9F)
+        Label1.Location = New Point(12, 79)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(159, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "Username (OPTIONAL)"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Semibold", 20F, FontStyle.Bold)
+        Label2.Location = New Point(12, 22)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(476, 46)
+        Label2.TabIndex = 2
+        Label2.Text = "STUDENT FEEDBACK SYSTEM"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.Font = New Font("Segoe UI", 12F)
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"IPROGLAB", "IPROGLEC", "DISSTRU"})
+        ComboBox1.Location = New Point(12, 182)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(300, 36)
+        ComboBox1.TabIndex = 3
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 159)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(58, 20)
+        Label3.TabIndex = 4
+        Label3.Text = "Subject"
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.Font = New Font("Segoe UI", 12F)
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Items.AddRange(New Object() {"Mr. Jovel Advincula", "Mrs. Sheila Marie Matias", "Mr. Al Santiago"})
+        ComboBox2.Location = New Point(12, 254)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(300, 36)
+        ComboBox2.TabIndex = 5
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(12, 231)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(71, 20)
+        Label4.TabIndex = 6
+        Label4.Text = "Instructor"
+        ' 
+        ' ComboBox3
+        ' 
+        ComboBox3.Font = New Font("Segoe UI", 12F)
+        ComboBox3.FormattingEnabled = True
+        ComboBox3.Items.AddRange(New Object() {"1", "2"})
+        ComboBox3.Location = New Point(12, 325)
+        ComboBox3.Name = "ComboBox3"
+        ComboBox3.Size = New Size(300, 36)
+        ComboBox3.TabIndex = 7
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(12, 302)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(70, 20)
+        Label5.TabIndex = 8
+        Label5.Text = "Semester"
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Font = New Font("Segoe UI", 10F)
+        RadioButton2.Location = New Point(327, 184)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(40, 27)
+        RadioButton2.TabIndex = 9
+        RadioButton2.Text = "1"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(364, 185)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(38, 24)
+        RadioButton1.TabIndex = 10
+        RadioButton1.Text = "2"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(406, 186)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(38, 24)
+        RadioButton3.TabIndex = 11
+        RadioButton3.Text = "3"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.Location = New Point(444, 186)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(38, 24)
+        RadioButton4.TabIndex = 12
+        RadioButton4.Text = "4"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton5
+        ' 
+        RadioButton5.AutoSize = True
+        RadioButton5.Location = New Point(482, 186)
+        RadioButton5.Name = "RadioButton5"
+        RadioButton5.Size = New Size(38, 24)
+        RadioButton5.TabIndex = 13
+        RadioButton5.Text = "5"
+        RadioButton5.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Segoe UI", 15F)
+        TextBox2.Location = New Point(12, 394)
+        TextBox2.MaxLength = 2000
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.PlaceholderText = "Text here.."
+        TextBox2.Size = New Size(300, 43)
+        TextBox2.TabIndex = 21
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(12, 371)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(158, 20)
+        Label8.TabIndex = 22
+        Label8.Text = "Comment (OPTIONAL)"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton9)
+        GroupBox1.Controls.Add(RadioButton10)
+        GroupBox1.Controls.Add(RadioButton6)
+        GroupBox1.Controls.Add(RadioButton8)
+        GroupBox1.Controls.Add(RadioButton7)
+        GroupBox1.Location = New Point(318, 237)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(210, 53)
+        GroupBox1.TabIndex = 23
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Rate Instructor"
+        ' 
+        ' RadioButton9
+        ' 
+        RadioButton9.AutoSize = True
+        RadioButton9.Location = New Point(124, 23)
+        RadioButton9.Name = "RadioButton9"
+        RadioButton9.Size = New Size(38, 24)
+        RadioButton9.TabIndex = 27
+        RadioButton9.Text = "4"
+        RadioButton9.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton10
+        ' 
+        RadioButton10.AutoSize = True
+        RadioButton10.Location = New Point(164, 23)
+        RadioButton10.Name = "RadioButton10"
+        RadioButton10.Size = New Size(38, 24)
+        RadioButton10.TabIndex = 28
+        RadioButton10.Text = "5"
+        RadioButton10.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton6
+        ' 
+        RadioButton6.AutoSize = True
+        RadioButton6.Location = New Point(7, 23)
+        RadioButton6.Name = "RadioButton6"
+        RadioButton6.Size = New Size(38, 24)
+        RadioButton6.TabIndex = 24
+        RadioButton6.Text = "1"
+        RadioButton6.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton8
+        ' 
+        RadioButton8.AutoSize = True
+        RadioButton8.Location = New Point(86, 23)
+        RadioButton8.Name = "RadioButton8"
+        RadioButton8.Size = New Size(38, 24)
+        RadioButton8.TabIndex = 26
+        RadioButton8.Text = "3"
+        RadioButton8.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton7
+        ' 
+        RadioButton7.AutoSize = True
+        RadioButton7.Location = New Point(45, 23)
+        RadioButton7.Name = "RadioButton7"
+        RadioButton7.Size = New Size(38, 24)
+        RadioButton7.TabIndex = 25
+        RadioButton7.Text = "2"
+        RadioButton7.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Location = New Point(318, 159)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(210, 59)
+        GroupBox2.TabIndex = 24
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Rate Subject"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 7F)
+        Label6.Location = New Point(12, 440)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(145, 15)
+        Label6.TabIndex = 25
+        Label6.Text = "Character Counter: 0/2000"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 13F)
+        Button1.Location = New Point(23, 472)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(134, 53)
+        Button1.TabIndex = 26
+        Button1.Text = "Submit"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Segoe UI", 7F)
+        Label9.Location = New Point(534, 254)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(223, 90)
+        Label9.TabIndex = 28
+        Label9.Text = "Rate the Instructor based on their overall " & vbCrLf & "performance such as effectiveness, " & vbCrLf & "fairness, attitude, uses of materials " & vbCrLf & "and knowledge." & vbCrLf & vbCrLf & vbCrLf
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 13F)
+        Button2.Location = New Point(623, 472)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(134, 53)
+        Button2.TabIndex = 29
+        Button2.Text = "Return"
+        Button2.UseVisualStyleBackColor = True
         ' 
         ' Form2
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(789, 549)
+        Controls.Add(Button2)
+        Controls.Add(Label9)
+        Controls.Add(Button1)
+        Controls.Add(Label6)
+        Controls.Add(Label8)
+        Controls.Add(TextBox2)
+        Controls.Add(RadioButton5)
+        Controls.Add(RadioButton4)
+        Controls.Add(RadioButton3)
+        Controls.Add(RadioButton1)
+        Controls.Add(RadioButton2)
+        Controls.Add(Label5)
+        Controls.Add(ComboBox3)
+        Controls.Add(Label4)
+        Controls.Add(ComboBox2)
+        Controls.Add(Label3)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(GroupBox1)
+        Controls.Add(GroupBox2)
         Name = "Form2"
         Text = "Form2"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Private WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton9 As RadioButton
+    Friend WithEvents RadioButton10 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton8 As RadioButton
+    Friend WithEvents RadioButton7 As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Button2 As Button
 End Class
--- My Project/Form2.vb
+++ My Project/Form2.vb
@@ -1,3 +1,32 @@
 ﻿Public Class Form2
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
 
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged, RadioButton2.CheckedChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        Dim maxChars As Integer = 2000
+        Dim count As Integer = TextBox2.Text.Length
+
+        Label6.Text = "Character Counter: " & count & "/" & maxChars
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
 End Class
