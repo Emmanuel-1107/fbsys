4505572645144f99d072ed0aa11c3a1f6dc2cd48
Emmanuel <golondrina.136510120196@depedqc.ph>
2026-05-03 13:29:38 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 35F)
+        Label1.Location = New Point(43, 25)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(698, 78)
+        Label1.TabIndex = 0
+        Label1.Text = "Student Feedback System"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 20F)
+        Button1.Location = New Point(181, 175)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(418, 103)
+        Button1.TabIndex = 1
+        Button1.Text = "Give Feedback"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 20F)
+        Button2.Location = New Point(181, 338)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(418, 103)
+        Button2.TabIndex = 2
+        Button2.Text = "Admin Login"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(788, 547)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+
+End Class
